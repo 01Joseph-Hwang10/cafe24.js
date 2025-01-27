da5b83ffe0f528feeeb847e5dbe05bfdf1a012e5
01Joseph-Hwang10 <joseph95501@gmail.com>
2025-01-27 19:04:25 +0900

    fix: export base types

--- packages/cafe24-api-client/src/index.ts
+++ packages/cafe24-api-client/src/index.ts
@@ -3,6 +3,7 @@ import * as api from "./api";
 export default api;
 
 export type { RequestType, ResponseType } from "./utils/types";
+export * from "./api/client";
 export * from "./task-queue";
 export * from "./http-agent";
 export * from "./logging";
